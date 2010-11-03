d1786388a1664827842017c5c59d1af79eff9ad2
Henning Günther <h.guenther@tu-bs.de>
2010-11-03 15:26:57 +0100

    New dimension handling for iterations

--- Training.py
+++ Training.py
@@ -9,6 +9,7 @@ from GathGeva import GathGeva
 from Regression import linear_regression
 from evolve import evolve_fis
 import rule
+import math
 
 class TrainingState:
     """
@@ -29,23 +30,30 @@ class TrainingState:
         Create a fuzzy inference system using a genetic algorithm        
 
         :param iterations: The algorithm can use the results from the last iteration to stabilize the resulting FIS. Specifies the amount of iterations to perform.
+        :type iterations: :class:`int`
         :param cb: A callback function that is called with the progress encoded as a float from 0.0 to 1.0
         """
         classifiers = []
-        rng = self.max_range() - self.min_range()
+        #rng = self.max_range() - self.min_range()
         for i,cl_state in enumerate(self.classifier_states):
             best_fis = None
             best_quality = 0
             for it in range(iterations):
+                rng = self.max_range() - self.min_range()
+                for i in range(rng.shape[0]):
+                    if rng[i] == 0.0:
+                        rng[i] = 1.0
                 if cb:
                     cb((float(i)+float(it)/iterations) / len(self.classifier_states))
                 fis = cl_state.best_fis(rng)
                 if fis:
-                    quality,count = cl_state.quality_fis(fis,it!=0)
+                    quality,count = cl_state.quality_fis(fis)
                     print "Quality:",100.0*quality / count
                     if quality > best_quality:
                         best_fis = fis
-                    cl_state.adjust_data(fis,it==0)
+                    if it==0:
+                        cl_state.attach_dimension()
+                    cl_state.adjust_data(fis)
             if not best_fis:
                 return None
             classifiers.append(rule.Classifier(best_fis,cl_state.membership(),cl_state.name))
@@ -85,7 +93,7 @@ class ClassifierState:
                 means.append(ggres[2])
                 covars.append(ggres[3])
             ras = linear_regression(zip(means,covars),
-                                    [(cl.id,cl.training_data) for cl in self.classes])
+                                    [(cl.id,cl.get_training_data()) for cl in self.classes])
             print "Success"
             return build_classifier(means,covars,ras)
         except Exception as err:
@@ -98,10 +106,13 @@ class ClassifierState:
         :returns: A number representing the quality of the FIS. Higher is better.
         :return-type: :class:`float`
         """
-        res = 0.0
-        for cl_state in self.classes:
-            res += cl_state.eval_fis(fis)
-        return 1.0/res
+        #res = 0.0
+        #for cl_state in self.classes:
+        #    res += cl_state.eval_fis(fis)
+        #print "=>",res
+        #return 1.0/res
+        correct,count = self.quality_fis(fis)
+        return correct
     def best_fis(self,rng):
         """
         Calculate the best Fuzzy Inference System for this classifier using a genetic algorithm.
@@ -109,29 +120,33 @@ class ClassifierState:
         :param rng: The normalization factor for the training data
         """
         return evolve_fis(self.clusters(rng),self.gen_fis,self.eval_fis)
-    def quality_fis(self,fis,attach_dim=False):
+    def quality_fis(self,fis):
         """
         Get a quality estimate for a FIS by counting the correct classifications on the check data.
 
         :returns: A tuple with the correct classifications and the total number of check-datasets
-        :return-type: (:class:`int`,:class:`int`)
+        :return-type: (:class:`int`, :class:`int`)
         """
         correct = 0
         count = 0
         for cl_state in self.classes:
-            r,c = cl_state.quality_fis(fis,attach_dim)
+            r,c = cl_state.quality_fis(fis)
+            print "For",cl_state.name,r,"/",c
             correct += r
             count += c
         return (correct,count)
-    def adjust_data(self,fis,attach_dim=False):
+    def adjust_data(self,fis):
         for cl_state in self.classes:
-            cl_state.adjust_data(fis,attach_dim)
+            cl_state.adjust_data(fis)
     def membership(self):
         return [(cl.name,cl.id) for cl in self.classes if cl.name!=None]
     def min_range(self):
         return np.min([cl.min_range() for cl in self.classes],0)
     def max_range(self):
         return np.min([cl.max_range() for cl in self.classes],0)
+    def attach_dimension(self):
+        for cl_state in self.classes:
+            cl_state.attach_dimension()
 
 class ClassState:
     """
@@ -140,8 +155,10 @@ class ClassState:
     def __init__(self,name,id,tr_dat,ch_dat=None):
         self.name = name
         self.id = id
-        self.training_data = tr_dat
+        #self.training_data = tr_dat
+        self.training_data = np.hstack((tr_dat,np.zeros((tr_dat.shape[0],1))))
         self.check_data = ch_dat
+        self.extended = False
     def clusters(self,rng):
         """
         Use the subclustering algorithm to calculate initial clusters for this class from the training data
@@ -150,49 +167,84 @@ class ClassState:
         :type rng: :class:`float`
         """
         #clusts = subclust(normalize(self.training_data),0.4,0.5)
-        clusts = subclust(self.training_data / rng,0.4,0.5)
+        if self.extended:
+            dat = self.training_data / rng
+        else:
+            dat = self.training_data[:,0:-1] / rng[0:-1]
+
+        clusts = subclust(normalize(dat),0.1,0.15,0.1,0.05) #0.4 0.5
+
         print len(clusts),"initial clusters for class",self.name
-        return np.array([self.training_data[i] for i in clusts])
+        if self.extended:
+            return np.array([self.training_data[i] for i in clusts])
+        else:
+            return np.array([self.training_data[i,0:-1] for i in clusts])
     def gath_geva(self,vec):
         """
         Perform the Gath-Geva clustering algorithm on the training data using an initial state
 
         :param vec: The initial state for the algorithm
         """
-        return GathGeva(self.training_data,vec)
+        if self.extended:
+            return GathGeva(self.training_data,vec)
+        else:
+            return GathGeva(self.training_data[:,0:-1],vec)
     def eval_fis(self,fis):
-        delt = self.id - fis.evaluates(self.training_data)
+        if fis.dimension() == self.training_data.shape[1]:
+            delt = self.id - fis.evaluates(self.training_data)
+        else:
+            delt = self.id - fis.evaluates(self.training_data[:,0:-1])
+        print ma.count_masked(ma.masked_inside(delt,-0.5,0.5)),"/",self.training_data.shape[0]
         return np.sum(delt*delt) / self.training_data.shape[0]
-    def quality_fis(self,fis,attach_dim=False):
+    def quality_fis(self,fis):
         """
         Count the correct classifications of a given FIS on the check data.
 
         :param fis: The Fuzzy Inference System to be tested
-        :param attach_dim: Does the 
+        :type fis: :class:`rule.ClassifierSet`
+        :returns: A tuple containing the number of correct classifications and the total number of classifications
+        :rtype: (:class:`int`,:class:`int`)
         """
-        if attach_dim:
+        if fis.dimension() == self.training_data.shape[1]:
             dat = np.hstack((self.check_data,np.zeros((self.check_data.shape[0],1))))
         else:
             dat = self.check_data
+        #if self.check_data.shape[1] == self.training_data.shape[1]:
+        #    dat = self.check_data
+        #else:
+        #    dat = np.hstack((self.check_data,np.zeros((self.check_data.shape[0],1))))
         rvec = fis.evaluates(dat) - self.id
         rvec = ma.masked_inside(rvec,-0.5,0.5)
         return (ma.count_masked(rvec),self.check_data.shape[0])
-    def adjust_data(self,fis,attach_dim=False):
+    def adjust_data(self,fis):
         """
         Use a FIS to adjust the last dimension of the training data with the results of the evaluation.
 
         :param fis: The Fuzzy Inference System to be used.
-        :param attach_dim: Attach a new dimension to the training data or use the existing last one.
         """
-        res = fis.evaluates(self.training_data)
-        if attach_dim:
-            self.training_data = np.concatenate((self.training_data,np.array([res]).T),1)
-        else:
-            self.training_data[:,-1] = res
+        last_res = 0.0
+        for i in range(self.training_data.shape[0]):
+            if fis.dimension() == self.training_data.shape[1]:
+                last_res = fis.evaluate(np.hstack((self.training_data[i,0:-1],last_res)))
+            else:
+                last_res = fis.evaluate(self.training_data[i,0:-1])
+            if math.isnan(last_res):
+                self.training_data[i,-1] = 0.0
+            else:
+                self.training_data[i,-1] = last_res
     def min_range(self):
         return np.min(self.training_data,0)
     def max_range(self):
         return np.max(self.training_data,0)
+    def attach_dimension(self):
+        print "Attaching new dimension"
+        self.extended = True
+        #self.training_data = np.hstack((self.training_data,np.zeros((self.training_data.shape[0],1))))
+    def get_training_data(self):
+        if self.extended:
+            return self.training_data
+        else:
+            return self.training_data[:,0:-1]
 
 def build_classifier(means,covars,ras):
     rules = []
