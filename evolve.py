2220fa470ca80dae22885f238aea85e1b1484639
Henning Günther <h.guenther@tu-bs.de>
2010-11-03 15:28:55 +0100

    Introduce cache to speed up genetic vector selection

--- evolve.py
+++ evolve.py
@@ -3,31 +3,38 @@ from pyevolve import GSimpleGA
 import numpy as np
 
 def evolve_fis(vecs,gen_fis,eval_fis):
+    cache = {}
     size = 0
     for vec in vecs:
         size += vec.shape[0]
     genome = G1DBinaryString.G1DBinaryString(size)
     
     def loc_fitness(bitvec):
+        if bitvec.getDecimal() in cache:
+            return cache[bitvec.getDecimal()]
         offset = 0
         rvecs = []
         for vec in vecs:
             sel_vec = np.array([vec[i] for i in range(vec.shape[0]) if bitvec[offset+i]==1])
             if sel_vec.shape[0] == 0:
+                cache[bitvec.getDecimal()] = 0.0
                 return 0.0
             rvecs.append(sel_vec)
+            offset += vec.shape[0]
         fis = gen_fis(rvecs)
         if fis is None:
-            return 0.0
+            res = 0.0
         else:
-            return eval_fis(fis)
+            res = eval_fis(fis)
+        cache[bitvec.getDecimal()] = res
+        return res
 
     genome.evaluator.set(loc_fitness)
 
     ga = GSimpleGA.GSimpleGA(genome,interactiveMode=True)
     ga.setGenerations(10)
     ga.setPopulationSize(10)
-    ga.terminationCriteria.set(GSimpleGA.ConvergenceCriteria)
+    #ga.terminationCriteria.set(GSimpleGA.ConvergenceCriteria)
     ga.evolve(freq_stats=1)
 
     bitvec = ga.bestIndividual()
