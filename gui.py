a63d911a08fd4d13f972a58f8f29bd29d91448aa
Henning Günther <h.guenther@tu-bs.de>
2010-12-16 16:36:03 +0100

    Create GUI for parameter input

--- gui.py
+++ gui.py
@@ -11,17 +11,18 @@ import Training
 gobject.threads_init()
 
 class TrainingProgress(threading.Thread):
-    def __init__(self,mp,training_data,progress=None,done=None):
+    def __init__(self,mp,training_data,progress=None,done=None,parameter=Training.TrainingParameter()):
         threading.Thread.__init__(self)
         self.mp = mp
         self.training_data = training_data
         self.progress = progress
         self.done = done
+        self.parameter = parameter
     def cb(self,pc):
         if self.progress is not None:
             self.progress.set_fraction(pc)
     def run(self):
-        state = Training.TrainingState()
+        state = Training.TrainingState(self.parameter)
         self.cls_id = 0
         idx = 1
         for key,val in self.mp.iteritems():
@@ -62,9 +63,9 @@ class TrainingProgress(threading.Thread):
             #self.cls_id += 1
             state.add_classifier_state(classifier_state)
         if self.done is not None:
-            self.done(state.buildFIS(iterations=5,cb=self.cb))
+            self.done(state.buildFIS(cb=self.cb))
         else:
-            state.buildFIS(iterations=5,cb=self.cb)
+            state.buildFIS(cb=self.cb)
         
 
 class ContextTrainer(gtk.Assistant):
@@ -75,6 +76,7 @@ class ContextTrainer(gtk.Assistant):
     def __init__(self):
         gtk.Assistant.__init__(self)
         self.create_page_training_set()
+        self.create_page_parameter()
         self.create_page_classifier_preperation()
         self.create_page_generating()
         self.create_page_save()
@@ -109,6 +111,71 @@ class ContextTrainer(gtk.Assistant):
         self.set_page_title(p1,"Data selection")
         self.chooser = training_data_selector
         self.page_training_set = p1
+    def create_page_parameter(self,default=Training.TrainingParameter()):
+        page = gtk.Table(6,3)
+        page.set_col_spacing(0,15)
+        lbl_evolution = gtk.Label()
+        lbl_evolution.set_markup('<b>FIS evolution</b>')
+        lbl_evolution.set_alignment(0.0,0.5)
+        page.attach(lbl_evolution,0,3,0,1)
+
+        lbl_iterations = gtk.Label('Iterations:')
+        lbl_iterations.set_alignment(0.0,0.5)
+        page.attach(lbl_iterations,1,2,1,2)
+
+        entry_iterations = gtk.SpinButton()
+        entry_iterations.set_range(1,100)
+        entry_iterations.set_numeric(True)
+        entry_iterations.set_increments(1,5)
+        entry_iterations.set_value(default.gg_iterations)
+        page.attach(entry_iterations,2,3,1,2)
+        
+        lbl_generations = gtk.Label('Generations:')
+        lbl_generations.set_alignment(0.0,0.5)
+        page.attach(lbl_generations,1,2,2,3)
+
+        entry_generations = gtk.SpinButton()
+        entry_generations.set_range(1,100)
+        entry_generations.set_numeric(True)
+        entry_generations.set_increments(1,5)
+        entry_generations.set_value(default.gg_generations)
+        page.attach(entry_generations,2,3,2,3)
+
+        lbl_popsize = gtk.Label('Population size:')
+        lbl_popsize.set_alignment(0.0,0.5)
+        page.attach(lbl_popsize,1,2,3,4)
+
+        entry_popsize = gtk.SpinButton()
+        entry_popsize.set_range(1,200)
+        entry_popsize.set_numeric(True)
+        entry_popsize.set_increments(1,5)
+        entry_popsize.set_value(default.gg_population_size)
+        page.attach(entry_popsize,2,3,3,4)
+
+        lbl_training_data = gtk.Label()
+        lbl_training_data.set_markup('<b>Training data</b>')
+        lbl_training_data.set_alignment(0.0,0.5)
+        page.attach(lbl_training_data,0,3,4,5)
+
+        lbl_shuffle = gtk.Label('Shuffle size:')
+        lbl_shuffle.set_alignment(0.0,0.5)
+        page.attach(lbl_shuffle,1,2,5,6)
+
+        entry_shuffle = gtk.SpinButton()
+        entry_shuffle.set_range(1,200)
+        entry_shuffle.set_numeric(True)
+        entry_shuffle.set_increments(1,5)
+        entry_shuffle.set_value(default.shuffle_size)
+        page.attach(entry_shuffle,2,3,5,6)
+        
+        self.append_page(page)
+        self.set_page_title(page,"Algorithm Parameter")
+        self.page_parameter = page
+
+        self.entry_iterations = entry_iterations
+        self.entry_popsize = entry_popsize
+        self.entry_generations = entry_generations
+        self.entry_shuffle = entry_shuffle
     def chooser_changed(self,fc):
         self.training_data = None
         self.set_page_complete(self.page_training_set,True)
@@ -125,14 +192,20 @@ class ContextTrainer(gtk.Assistant):
     def prepare_page(self,ass,page):
         if page == self.page_training_set:
             pass
+        elif page == self.page_parameter:
+            self.set_page_complete(self.page_parameter,True)
         elif page == self.page_classifier_preperation:
+            self.parameter = Training.TrainingParameter(gg_iterations=self.entry_iterations.get_value(),
+                                                        gg_generations=self.entry_generations.get_value(),
+                                                        gg_popsize=self.entry_popsize.get_value(),
+                                                        shuffle_size=self.entry_shuffle.get_value())
             if self.training_data is None:
                 self.load_ann_pkg()
                 self.model = ClassifierAssoc(self.training_data.keys())
                 self.tv.set_model(self.model)
             self.set_page_complete(self.page_classifier_preperation,True)
         elif page == self.page_generating:
-            thr = TrainingProgress(self.model.get_mapping(),self.training_data,self.progress,self.finish_3)
+            thr = TrainingProgress(self.model.get_mapping(),self.training_data,self.progress,self.finish_3,self.parameter)
             thr.start()
             
             
