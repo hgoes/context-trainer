42fe9fc10f642fa826608b5b654442b07297693d
Henning Günther <h.guenther@tu-bs.de>
2011-02-01 13:03:27 +0100

    Expose parameters to the command line

--- command_line.py
+++ command_line.py
@@ -4,12 +4,19 @@ from annpkg.model import AnnPkg
 import gui
 import json
 import numpy as np
+import Training
 
 gobject.threads_init()
 
 training_file = sys.argv[1]
 classification = sys.argv[2]
 target_file = sys.argv[3]
+
+if len(sys.argv) > 4:
+    with open(sys.argv[4]) as h:
+      parameter = Training.TrainingParameter.from_json(json.load(h))
+else:
+    parameter = Training.TrainingParameter()
 
 mapping = {}
 for classifier in classification.split("#"):
@@ -29,6 +36,6 @@ def done(fis):
     with open(target_file,'w') as h:
         json.dump(fis.to_json(),h,indent=True)
 
-thread = gui.TrainingProgress(mapping,mp,done=done)
+thread = gui.TrainingProgress(mapping,mp,done=done,parameter=parameter)
 thread.start()
 thread.join()
